b49317e4657b1180a60a7c23715082dfcb877c66
pakcyeong <p1105_00@naver.com>
2022-03-22 08:47:33 +0900

    1.0.0

--- app/DiscordBotLoa.js
+++ app/DiscordBotLoa.js
@@ -44,7 +44,7 @@ class DiscordBotLoa extends Client {
 
         });
 
-        this.login('OTQ4MDA4MzQ2ODUyNjYzMzE2.Yh1jkw.Z0FJ120Vmt-B6FIDSv3AQOSAQbk');
+        this.login(process.env.TOKEN);
     }
 
 }
