1ea4f4217fc0d4ae2f1ad0d11b378c74e4156685
pakcyeong <p1105_00@naver.com>
2022-03-28 07:53:22 +0900

    test

--- app/DiscordBotLoa.js
+++ app/DiscordBotLoa.js
@@ -55,7 +55,7 @@ class DiscordBotLoa extends Client {
                     },
                 };
                 
-                message.channel.send({ embeds: [exampleEmbed], files: [file] });
+                message.channel.send({ embeds: [exampleEmbed] });
             }else{
                 return;
             }
